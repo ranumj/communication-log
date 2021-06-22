d3b9801a995d0c801c675c9ae4f887e4ea41b537
ranumj <gman.0505@gmail.com>
2021-06-22 15:57:29 -0500

    Added enter name feature

--- communication-log/communication-log/Program.cs
+++ communication-log/communication-log/Program.cs
@@ -10,6 +10,10 @@ namespace communication_log
             Console.WriteLine("Hello control!");
             Console.WriteLine("Wow it's actually working!");
             Console.WriteLine("Just wait until Microsoft sees this! We're basically engineers now.");
+
+            Console.WriteLine("Please enter your name.");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Your name is {input}.");
         }
     }
 }
