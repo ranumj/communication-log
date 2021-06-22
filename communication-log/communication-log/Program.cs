9439054844a145db4faae99f9122234ac18a70b7
Hunter Hrab <hunter.hrab@gmail.com>
2021-06-22 16:35:32 -0400

    updated for better job offer

--- communication-log/communication-log/Program.cs
+++ communication-log/communication-log/Program.cs
@@ -9,6 +9,7 @@ namespace communication_log
             Console.WriteLine("Hello pilot!");
             Console.WriteLine("Hello control!");
             Console.WriteLine("Wow it's actually working!");
+            Console.WriteLine("Just wait until Microsoft sees this! We're basically engineers now.");
         }
     }
 }
